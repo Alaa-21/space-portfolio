70f7de6af140ba4c44488c0fc90d4c1cdc2094f6
Alaa-21 <alaanasser053@gmail.com>
2024-01-27 20:49:41 +0200

    laaa

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border h-[23rem] border-[#2a0e61]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border h-[22rem] border-[#2a0e61]">
       <Image
         src={src}
         alt={title}
@@ -17,7 +17,7 @@ const ProjectCard = ({ src, title, description }: Props) => {
         className="w-full object-cover"
       />
 
-      <div className="relative -top- p-4">
+      <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
       </div>
